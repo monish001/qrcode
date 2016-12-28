e341e92fb9b09600c31d39d1ce27a873df236593
Monish Gupta <monish.gupta1@gmail.com>
2016-12-28 10:18:26 +0530

    Frame decoding moved to web worker

--- app/scripts/qrclient.js
+++ app/scripts/qrclient.js
@@ -1,14 +1,32 @@
 var QRClient = function() {
 
   var currentCallback;
-  
+  var imageWorker = new Worker('scripts/jsqrcode/qrworker.js');
+      imageWorker.onmessage = function(e){
+        if(e.data) {
+          debugger;
+          currentCallback(e.data);
+        }
+      };
+      imageWorker.onerror = function(error){
+        function WorkerException(message){
+          this.name = 'WorkerException';
+          this.message = message;
+        }
+        throw new WorkerException('Worker error.');
+      };
+
+
   this.decode = function(imageData, callback) {
     try {
-      var width = imageData.width;
-      var height = imageData.height;
-      var result = qrcode.decode(width, height, imageData);
-      callback(result);
-    } 
+      currentCallback = callback;
+      imageWorker.postMessage({
+        data: imageData.data,
+        height: imageData.height,
+        width: imageData.width
+      });
+
+    }
     catch(e) {
       // consume the error.
       console.log(e)
